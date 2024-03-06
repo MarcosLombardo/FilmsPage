83d2ab6722fe993146de79a11774e72cde34b26a
MarcosLombardo <marcoslombardo28@gmail.com>
2024-03-06 09:59:57 -0300

    Pagina perteneciente al modulo2. Primeros pasos. Estructura HTML, estilos CSS e interactividad con JS

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,24 @@
-console.log(tempData);
+const filmSection = document.getElementById('films');
+
+function renderFilms (movie) {
+    const containerMovie = document.createElement('div');
+    containerMovie.classList.add('tarjeta');
+
+    containerMovie.innerHTML = `<div class="face front">
+                                <img src="${movie.poster}" alt="${movie.title}">
+                                </div>
+                                <div class="face back">
+                                <h3>${movie.title} (${movie.year})</h3>
+                                <div class="separador">
+                                </div>
+                                <p>${movie.duration}</p>
+                                <p>${movie.director}</p>
+                                <p>${movie.genre}</p>
+                                <p>${movie.rate}</p>
+                                </div> `;
+
+    filmSection.appendChild(containerMovie);
+
+}
+
+tempData.forEach(renderFilms);
