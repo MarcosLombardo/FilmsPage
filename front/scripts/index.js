5a9fcffcd5a3ffcf5beb9a45101c6a72ce0390a2
MarcosLombardo <marcoslombardo28@gmail.com>
2024-03-11 16:52:47 -0300

    Homework clase 06 - Agregar AXIOS y renderizar las tarjetas de peliculas

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,3 +1,39 @@
 const renderFilms = require('./renderCards.js');
+const axios = require('axios');
 
-$.get("https://students-api.2.us-1.fl0.io/movies", (data)=> data.forEach(renderFilms));
+/* --------------------------------------------- Renderizar la API con jQuery --------------------------------------------- */
+
+// $.get("https://students-api.2.us-1.fl0.io/movies", (data)=> data.forEach(renderFilms));
+
+
+/* --------------------------------------------- Renderizar la API con una promesa --------------------------------------------- */
+
+// const promise = axios.get("https://students-api.2.us-1.fl0.io/movies");
+
+// promise
+//     .then((res) => {
+//         const movies = res.data;
+//         movies.forEach(movie => {
+//             renderFilms(movie);
+//         });
+//     })
+//     .catch((err) => {
+//         console.error("No se encuentra la API");
+//     });
+
+/* --------------------------------------------- Renderizar la API con async/await --------------------------------------------- */
+
+
+const fetchMovies = async () => {
+    try {
+        const data = await axios.get("https://students-api.2.us-1.fl0.io/movies");
+        const movies = res.data;
+            movies.forEach(movie => {
+            renderFilms(movie);
+        });
+    } catch (err) {
+        console.error("No se encuentra la API");
+    }   
+}
+
+fetchMovies();
